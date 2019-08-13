4b7a5e6ddc1e8ed4fabb2a80312ed1956dc28327
hamdle <contact@ericmarty.co>
2019-08-12 23:28:14 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 20, 2019
+ *      Author: eric
+ */
+
+#include <SDL/SDL.h>
+
+int main(int arc, char* args[])
+{
+	SDL_Init( SDL_INIT_EVERYTHING );
+	printf("Linux FTW!!!");
+	SDL_Quit();
+
+	return 0;
+}
