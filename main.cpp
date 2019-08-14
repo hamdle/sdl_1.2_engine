9792c3146f89eb80ff30f51213d4068856e3a99c
hamdle <contact@ericmarty.co>
2019-08-13 21:35:15 -0700

    Display an image

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@
 
 #include <SDL/SDL.h>
 
+SDL_Surface* image = NULL;
+SDL_Surface* backbuffer = NULL;
+
 int main(int arc, char* args[])
 {
+	// Init SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 		printf("SDL failed to initialize!");
@@ -16,9 +20,27 @@ int main(int arc, char* args[])
 		return 1;
 	}
 
-	SDL_SetVideoMode(800, 600, 32, SDL_SWSURFACE);
+	backbuffer = SDL_SetVideoMode(800, 600, 32, SDL_SWSURFACE);
 	SDL_WM_SetCaption("SDL 1.2", NULL);
+
+	// Load the image
+	image = SDL_LoadBMP("graphics/image.bmp");
+
+	if (image == NULL)
+	{
+		printf("Image failed to load\n");
+		SDL_Quit();
+		return 1;
+	}
+
+	// Draw the image
+	SDL_BlitSurface(image, NULL, backbuffer, NULL);
+	SDL_Flip(backbuffer);
+
+	// Wait
 	SDL_Delay(3000);
+
+	SDL_FreeSurface(image);
 	SDL_Quit();
 
 	return 0;
