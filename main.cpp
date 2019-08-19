d3cb7f4b83646071e15e84ddd000b9dd81d52c24
hamdle <contact@ericmarty.co>
2019-08-18 18:56:30 -0700

    Create game loop

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@
 
 #include <SDL/SDL.h>
 
-SDL_Surface* image = NULL;
+SDL_Surface* background = NULL;
+SDL_Surface* sprite = NULL;
 SDL_Surface* backbuffer = NULL;
+
+bool ProgramIsRunning();
+bool LoadImages();
+void FreeImages();
 
 int main(int arc, char* args[])
 {
@@ -23,25 +28,78 @@ int main(int arc, char* args[])
 	backbuffer = SDL_SetVideoMode(800, 600, 32, SDL_SWSURFACE);
 	SDL_WM_SetCaption("SDL 1.2", NULL);
 
-	// Load the image
-	image = SDL_LoadBMP("graphics/image.bmp");
-
-	if (image == NULL)
+	if (!LoadImages())
 	{
-		printf("Image failed to load\n");
+		printf("Images failed to load\n");
+		FreeImages();
 		SDL_Quit();
 		return 1;
 	}
 
-	// Draw the image
-	SDL_BlitSurface(image, NULL, backbuffer, NULL);
-	SDL_Flip(backbuffer);
+	SDL_BlitSurface(background, NULL, backbuffer, NULL);
 
-	// Wait
-	SDL_Delay(3000);
+	while (ProgramIsRunning())
+	{
+		SDL_Rect spritePos;
+		spritePos.x = rand() % 800;
+		spritePos.y = rand() % 600;
 
-	SDL_FreeSurface(image);
+		SDL_BlitSurface(sprite, NULL, backbuffer, &spritePos);
+		SDL_Flip(backbuffer);
+		SDL_Delay(100);
+	}
+
 	SDL_Quit();
 
 	return 0;
+}
+
+bool ProgramIsRunning()
+{
+	SDL_Event event;
+	bool running = true;
+
+	while (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_QUIT)
+		{
+			running = false;
+		}
+	}
+
+	return running;
+}
+
+bool LoadImages()
+{
+	background = SDL_LoadBMP("graphics/background.bmp");
+
+	if (background == NULL)
+	{
+		return false;
+	}
+
+	sprite = SDL_LoadBMP("graphics/sprite.bmp");
+
+	if (sprite == NULL)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void FreeImages()
+{
+	if (background != NULL)
+	{
+		SDL_FreeSurface(background);
+		background = NULL;
+	}
+
+	if (sprite != NULL)
+	{
+		SDL_FreeSurface(sprite);
+		sprite = NULL;
+	}
 }
